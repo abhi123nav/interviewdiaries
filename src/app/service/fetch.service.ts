8cac0a7907eae24beb9c183ba5b0bc1f6c3b1f1b
abhi123nav <51525222+abhi123nav@users.noreply.github.com>
2021-06-22 23:13:16 +0530

    changes6

--- src/app/service/fetch.service.ts
+++ src/app/service/fetch.service.ts
@@ -3,21 +3,25 @@ import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 import { User } from '../user';
+import { environment } from 'src/environments/environment.prod';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class FetchService {
-  baseUrl='http://localhost:8082/cnames';
+  //baseUrl='http://localhost:8082/cnames';
+  baseUrl=environment.apiBaseURL;
   constructor(private http:HttpClient) { }
   public getData(){
-    return this.http.get("http://localhost:8082/cnames");
+    //return this.http.get("http://localhost:8082/cnames");
+    return this.http.get(this.baseUrl+'cnames/');
   }
   public getDatabycname(name:any):Observable<any>{
     // return this.http.get("http://localhost:8082/cnames/"+cname);
     // return this.http.get("http://localhost:8082/cnames/Wipro");
     // return this.http.get<User[]>(`${this.baseUrl}/${cname}`);
-    return this.http.get<User>('http://localhost:8082/cnames/'+name);
+   //this was working// return this.http.get<User>('http://localhost:8082/cnames/'+name);
+    return this.http.get<User>(this.baseUrl+'cnames/'+name);
   }
 }
