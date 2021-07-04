7402b51853687685cb4dd2f78a8c36fd723cc698
abhi123nav <51525222+abhi123nav@users.noreply.github.com>
2021-07-04 10:13:17 +0530

    changes

--- src/app/components/insertdata/insertdata.component.ts
+++ src/app/components/insertdata/insertdata.component.ts
@@ -10,8 +10,8 @@ import { User } from 'src/app/user';
 export class InsertdataComponent implements OnInit {
 
   states: string[] = [
-    'Accenture', 'ADP', 'Bridgei2i', 'Byjus', 'Deloitte', 'Exxon_mobil', 'Goldman_sachs', 'Infosys',
-    'Intellipaat', 'IQVIA', 'Itd_cementation', 'L&T', 'Maybank', 'Scaler_academy', 'Tata_project', 'TCS', 'Tcs_innovators',
+    'Accenture', 'ADP', 'Bridgei2i', 'Byjus', 'Deloitte', 'Exxonmobil', 'Goldmansachs', 'Infosys',
+    'Intellipaat', 'IQVIA', 'Itdcementation', 'Landt', 'Maybank', 'Scaleracademy', 'Tataproject', 'TCS', 'Tcsinnovators',
     'Wipro', 'WSP'
   ];
 
